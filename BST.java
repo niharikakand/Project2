266ca32fd432a4809b65483c599e336a2ae7ca39
jenna252 <jodom9504@gmail.com>
2024-03-14 09:52:59 -0400

    Created the package private Node class within the BST.file package. included constructors and getter and setter methods. - Jenna

--- BST.java
+++ BST.java
@@ -1,6 +1,119 @@
 /**
- *
+ * binary search class.
  */
 public class BST {
 
+}
+
+/**
+ * node class to be referenced by BST.
+ */
+class Node{
+    private int key;
+    private Node parent;
+    private Node leftChild;
+    private Node rightChild;
+
+    /**
+     * This is the default node constructor.
+     * Instances within the class are initialized.
+     * Key is intiialized to -1. All others are intialized to null.
+     */
+    Node(){
+        this.key = -1;
+        this.parent = null;
+        this.leftChild = null;
+        this.rightChild = null;
+    }
+
+    /**
+     * Creates new node where the key is initialized and all other fields are null.
+     * @param key - takes the integer version of key
+     */
+    Node(int key){
+        this.key = key;
+        this.parent = null;
+        this.leftChild = null;
+        this.rightChild = null;
+    }
+
+    /**
+     * Creates new node where all the fields are initialized according to parameters.
+     * @param key int
+     * @param parent is parent of Node being created
+     * @param leftChild of Node
+     * @param rightChild of Node
+     */
+    Node(int key, Node parent, Node leftChild, Node rightChild){
+        this.key = key;
+        this.parent = parent;
+        this.leftChild = leftChild;
+        this.rightChild = rightChild;
+    }
+
+    //getters and setters for the instance variables
+
+    /**
+     * gets Key.
+     * @return int key
+     */
+    int getKey(){
+        return this.key;
+    }
+
+    /**
+     * gets the parent Node.
+     * @return Node parent
+     */
+    Node getParent(){
+        return this.parent;
+    }
+
+    /**
+     * gets the leftChild of the Node
+     * @return Node leftChild
+     */
+    Node getLeftChild(){
+        return this.leftChild;
+    }
+
+    /**
+     * gets the rightChild of the Node.
+     * @return Node rightChild
+     */
+    Node getRightChild(){
+        return this.rightChild;
+    }
+
+    //setter methods
+    /**
+     * sets the key for Node.
+     * @param key int
+     */
+    void setKey(int key){
+        this.key = key;
+    }
+    /**
+     * sets the parent node of the Node.
+     * @param parent Node
+     */
+    void setParent(Node parent){
+        this.parent = parent;
+    }
+
+    /**
+     * sets the leftChild of the Node.
+     * @param leftChild Node
+     */
+    void setLeftChild(Node leftChild){
+        this.leftChild = leftChild;
+    }
+
+    /**
+     * sets the rightChild of the Node.
+     * @param rightChild Node
+     */
+    void setRightChild(Node rightChild){
+        this.rightChild = rightChild;
+    }
 }
